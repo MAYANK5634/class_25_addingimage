82af9c495449b008162e1b6a52042af44f3394b4
MAYANK5634 <71888040+MAYANK5634@users.noreply.github.com>
2020-10-19 20:03:46 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,10 @@
+class Log extends baseclass{
+    constructor(x, y, height, angle) {
+     
+this.image = loadImage ("sprites/wood2.png")
+      Matter.Body.setAngle(this.body, angle);
+
+    }
+   
+  };
+  
